1530811b68b6ec1e2da8a84cea1c452b1b299d52
william <william@WIlliamLinux>
2021-01-08 13:41:06 +0000

    added js reference to all form data

--- PhpProject1/lease-info-details.php
+++ PhpProject1/lease-info-details.php
@@ -13,6 +13,7 @@
 <head>
     <title>Lease Information Sheet</title>
     <?php include 'blocks/head.php' ?>
+    <script src="scripts/lease-info.js"></script>
 </head>
 <body>
 
@@ -52,7 +53,7 @@
                         <div class="input-group-prepend col-4">
                           <span class="input-group-text col-12" id="basic-addon3">Registered Owner</span>
                         </div>
-                        <input type="text" class="form-control col-sm-4" id="basic-url" aria-describedby="basic-addon3">
+                        <input type="text" id="ref-reg-owner" class="form-control col-sm-4" aria-describedby="basic-addon3">
                     </div>
             </div>
             <div class="form-group">
@@ -60,7 +61,7 @@
                     <div class="input-group-prepend col-4">
                       <span class="input-group-text col-12" id="basic-addon3">Address</span>
                     </div>
-                    <input type="text" class="form-control col-sm-4" id="basic-url" aria-describedby="basic-addon3">
+                    <input type="text" id="ref-address" class="form-control col-sm-4" aria-describedby="basic-addon3">
                 </div>
             </div>
             <div class="form-group">
@@ -68,7 +69,7 @@
                     <div class="input-group-prepend col-4">
                       <span class="input-group-text col-12" id="basic-addon3">Lease</span>
                     </div>
-                    <textarea class="form-control col-sm-4" id="basic-url" aria-label="With textarea"></textarea>
+                    <textarea class="form-control col-sm-4" id="ref-lease" aria-label="With textarea"></textarea>
                 </div>
             </div>
             <div class="form-group">
@@ -76,7 +77,7 @@
                     <div class="input-group-prepend col-4">
                       <span class="input-group-text col-12" id="basic-addon3">RTM/ Freeholder/ Manager</span>
                     </div>
-                    <input type="text" class="form-control col-sm-4" id="basic-url" aria-describedby="basic-addon3">
+                    <input type="text" class="form-control col-sm-4" id="ref-rtm" aria-describedby="basic-addon3">
                 </div>
             </div>
             <div class="form-group">
@@ -84,7 +85,7 @@
                     <div class="input-group-prepend col-4">
                       <span class="input-group-text col-12" id="basic-addon3">Rent</span>
                     </div>
-                    <input type="text" class="form-control col-sm-4" id="basic-url" aria-describedby="basic-addon3">
+                    <input type="text" class="form-control col-sm-4" id="ref-rent" aria-describedby="basic-addon3">
                 </div>
             </div>
             <div class="form-group">
@@ -92,8 +93,8 @@
                         <div class="input-group-prepend col-4">
                           <span class="input-group-text col-12" id="basic-addon3">Service Charges</span>
                         </div>
-                        <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
-                        <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+                        <input type="text" class="form-control" id="ref-service-charges" aria-describedby="basic-addon3">
+                        <input type="text" class="form-control" id="notes-service-charges" aria-describedby="basic-addon3">
                     </div>
             </div>
             <div class="form-group">
@@ -101,8 +102,8 @@
                     <div class="input-group-prepend col-4">
                       <span class="input-group-text col-12" id="basic-addon3">Interest</span>
                     </div>
-                    <textarea class="form-control" id="basic-url" aria-label="With textarea"></textarea>
-                    <textarea class="form-control" id="basic-url" aria-label="With textarea"></textarea>
+                    <textarea class="form-control" id="ref-interest" aria-label="With textarea"></textarea>
+                    <textarea class="form-control" id="notes-interest" aria-label="With textarea"></textarea>
                 </div>
             </div>
             <div class="form-group">
@@ -110,8 +111,8 @@
                     <div class="input-group-prepend col-4">
                       <span class="input-group-text col-12" id="basic-addon3">Costs</span>
                     </div>
-                    <textarea class="form-control" id="basic-url" aria-label="With textarea"></textarea>
-                    <textarea class="form-control" id="basic-url" aria-label="With textarea"></textarea>
+                    <textarea class="form-control" id="ref-costs" aria-label="With textarea"></textarea>
+                    <textarea class="form-control" id="notes-costs" aria-label="With textarea"></textarea>
                 </div>
             </div>
             <div class="form-group">
@@ -119,7 +120,7 @@
                     <div class="input-group-prepend col-4">
                       <span class="input-group-text col-12" id="basic-addon3">Forfeiture</span>
                     </div>
-                    <input type="text" class="form-control col-sm-4" id="basic-url" aria-describedby="basic-addon3">
+                    <input type="text" class="form-control col-sm-4" id="ref-forfeiture" aria-describedby="basic-addon3">
                 </div>
             </div>
             <div class="form-group">
@@ -127,8 +128,8 @@
                     <div class="input-group-prepend col-4">
                       <span class="input-group-text col-12" id="basic-addon3">Lender</span>
                     </div>
-                    <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
-                    <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+                    <input type="text" class="form-control" id="ref-lender" aria-describedby="basic-addon3">
+                    <input type="text" class="form-control" id="notes-lender" aria-describedby="basic-addon3">
                 </div>
             </div>
             <div class="form-group">
@@ -136,7 +137,7 @@
                     <div class="input-group-prepend col-4">
                       <span class="input-group-text col-12" id="basic-addon3">Correspondence Address</span>
                     </div>
-                    <textarea class="form-control col-sm-4" id="basic-url" aria-label="With textarea"></textarea>
+                    <textarea class="form-control col-sm-4" id="ref-correspondence-address" aria-label="With textarea"></textarea>
                 </div>
             </div>
             <div class="form-group">
@@ -144,8 +145,8 @@
                     <div class="input-group-prepend col-4">
                       <span class="input-group-text col-12" id="basic-addon3">Reminders</span>
                     </div>
-                    <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
-                    <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+                    <input type="text" class="form-control" id="ref-reminders" aria-describedby="basic-addon3">
+                    <input type="text" class="form-control" id="notes-reminders" aria-describedby="basic-addon3">
                 </div>
             </div>
       </form>
@@ -155,7 +156,7 @@
 </div>
     <div class="container" style="margin-bottom:20px">
         <div class="text-center">
-            <button type="button" class="btn btn-secondary btn-lg">Submit</button>
+            <button type="button" onclick="submit_form()" class="btn btn-secondary btn-lg">Submit</button>
         </div>
     </div>
 <div class="jumbotron text-center" style="margin-bottom:0">
---
+++ PhpProject1/scripts/lease-info.js
@@ -0,0 +1,28 @@
+
+
+
+function submit_form() {
+    
+    var ref_reg_owner = document.getElementById("ref-reg-owner").value;
+    var ref_address = document.getElementById("ref-address").value;
+    var ref_lease = document.getElementById("ref-lease").value;
+    var ref_rtm = document.getElementById("ref-rtm").value;
+    var ref_rent = document.getElementById("ref-rent").value;
+    var ref_service_charges = document.getElementById("ref-service-charges").value;
+    var notes_service_charges = document.getElementById("notes-service-charges").value;
+    var ref_interest = document.getElementById("ref-interest").value;
+    var notes_interest = document.getElementById("notes-interest").value;
+    var ref_costs = document.getElementById("ref-costs").value;
+    var notes_costs = document.getElementById("notes-costs").value;
+    var ref_forfeiture = document.getElementById("ref-forfeiture").value;
+    var ref_lender = document.getElementById("ref-lender").value;
+    var notes_lender = document.getElementById("notes-lender").value;
+    var ref_correspondence_address = document.getElementById("ref-correspondence-address").value;
+    var ref_reminders = document.getElementById("ref-reminders").value;
+    var notes_reminders = document.getElementById("notes-reminders").value;
+    
+    
+    
+    
+}
+
