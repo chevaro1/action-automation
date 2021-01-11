0eb44ba06c78c3b38773cd0a46e12772d3921e2f
william <w.galvin.info@gmail.com>
2021-01-11 15:13:57 +0000

    added tabulator

--- PhpProject1/calc-interest.php
+++ PhpProject1/calc-interest.php
@@ -1,1 +1,40 @@
-<h1>hello world</h1>
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document Automation</title>
+    <?php include 'blocks/head.php' ?>
+    <script src="scripts/required-docs.js"></script>
+</head>
+<body>
+
+<div class="jumbotron text-center" style="margin-bottom:0">
+  <h1>Document Automation</h1>
+  <p>Home Page</p>
+</div>
+
+<?php include "blocks/nav_bar.php" ?>
+
+<div class="container">
+    <div class="">
+        <div id="progressBar" style="height: 130px"></div>
+    </div>
+</div>
+
+
+
+<div class="jumbotron text-center" style="margin-bottom:0; margin-top:25px">
+  <p>Footer</p>
+</div>
+
+</body>
+</html>
