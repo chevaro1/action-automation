0ea0e49ae5fa3932410a42758d8548bf8afcd9de
william <w.galvin.info@gmail.com>
2021-01-12 17:38:56 +0000

    workeed on tabulator

--- blocks/head.php
+++ blocks/head.php
@@ -2,6 +2,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
   <link rel="stylesheet" href="css/progressBar.css">
   <link rel="stylesheet" href="css/loader.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -13,4 +14,5 @@
   <script src="http://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"> </script>
   <script src="scripts/progressBar.js"></script>
   <script src="libraries/progressStep.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
   <link rel="stylesheet" href="css/style.css">
--- scripts/calc-interest.js
+++ scripts/calc-interest.js
@@ -7,18 +7,70 @@
  	{id:5, name:"Margret Marmajuke", age:"16", col:"yellow", dob:"31/01/1999"},
  ];
 
+ var tabledata = [
+   {sdate:"01/01/2021", fdate:"", days:0, interestRate:0, dailyInterest:0, outstanding:0,interestPayable:0}
+ ];
+
+ //Create Date Editor
+ var dateEditor = function(cell, onRendered, success, cancel, editorParams){
+     var cellValue = cell.getValue(),
+     input = document.createElement("input");
+
+     input.setAttribute("type", "text");
+
+     input.style.padding = "4px";
+     input.style.width = "100%";
+     input.style.boxSizing = "border-box";
+
+     input.value = typeof cellValue !== "undefined" ? cellValue : "";
+
+     onRendered(function(){
+         input.style.height = "100%";
+         $(input).datepicker(); //turn input into datepicker
+         input.focus();
+     });
+
+     function onChange(e){
+         if(((cellValue === null || typeof cellValue === "undefined") && input.value !== "") || input.value != cellValue){
+             success(input.value);
+         }else{
+             cancel();
+         }
+     }
+
+     //submit new value on blur or change
+     input.addEventListener("change", onChange);
+     input.addEventListener("blur", onChange);
+
+     //submit new value on enter
+     input.addEventListener("keydown", function(e){
+         switch(e.keyCode){
+             case 13:
+             success(input.value);
+             break;
+
+             case 27:
+             cancel();
+             break;
+         }
+     });
+
+     return input;
+};
+
  //create Tabulator on DOM element with id "example-table"
  var table = new Tabulator("#example-table", {
   	height:205, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
   	data:tabledata, //assign data to table
   	layout:"fitColumns", //fit columns to width of table (optional)
+    reactiveData: true,
   	columns:[ //Define Table Columns
- 	 	{title:"Name", field:"name", width:150},
- 	 	{title:"Age", field:"age", hozAlign:"left", formatter:"progress"},
- 	 	{title:"Favourite Color", field:"col"},
- 	 	{title:"Date Of Birth", field:"dob", sorter:"date", hozAlign:"center"},
+ 	 	{title:"Start Date", field:"sdate", editor:dateEditor},
+ 	 	{title:"End Date", field:"fdate", sorter:"date", editor:dateEditor},
+ 	 	{title:"Days", field:"days"},
+ 	 	{title:"Interest Rate %", field:"interestRate", editor: true, cellEdited: function(cell)},
+    {title:"Daily Interest Rate", field:"dailyInterest"},
+    {title:"Amount Outstanding", field:"outstanding"},
+    {title:"Interest Payable", field:"interestPayable"}
   	],
-  	rowClick:function(e, row){ //trigger an alert message when the row is clicked
-  		alert("Row " + row.getData().id + " Clicked!!!!");
-  	},
  });
